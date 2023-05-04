40a6490a74919c9d68f3533d22c725fc6a83cd9a
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-05-04 11:21:37 +0300

    update

---
+++ src/views/Progress.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+import ProgressBar from '@/components/Progress/ProgressBar.vue';
+import ProgressCircle from '@/components/Progress/ProgressCircle.vue';
+import Button from '@/components/Button.vue';
+
+const percentBar = ref(40);
+const percentCircle = ref(75);
+
+const addPercentBar = () => {
+  percentBar.value += 5;
+  if (percentBar.value > 100) {
+    percentBar.value = 100;
+  }
+};
+const addPercentCircle = () => {
+  percentCircle.value += 5;
+  if (percentCircle.value > 100) {
+    percentCircle.value = 100;
+  }
+};
+</script>
+
+<template>
+  <h1 class="heading-1">Progress</h1>
+  <h2 class="heading-2">Progress Bar</h2>
+  <div class="line line_block">
+    <ProgressBar maxWidth="1000px" :percent="percentBar" color="success" />
+    <Button label="Add 5%" @click="addPercentBar" />
+  </div>
+  <h2 class="heading-2">Progress Circle</h2>
+  <div class="line line_block">
+    <ProgressCircle :percent="percentCircle" color="success" />
+    <Button label="Add 5%" @click="addPercentCircle" />
+  </div>
+</template>
---
+++ src/views/Tabs.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+import Tabs from '@/components/Tabs.vue';
+const tabs = [
+  { name: 'About', label: 'О проекте' },
+  { name: 'Vue', label: 'Про Vue' },
+  { name: 'React', label: 'Про React' },
+];
+
+const selectedTab = ref('Vue');
+const changeTab = (tabName) => {
+  selectedTab.value = tabName;
+};
+</script>
+
+<template>
+  <h1 class="heading-1">Tabs</h1>
+  <Tabs :selectedTab="selectedTab" :names="tabs" @changeTab="changeTab">
+    <div v-if="selectedTab === 'About'">Lorem ipsum dolor sit amet.</div>
+    <div v-if="selectedTab === 'Vue'">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, delectus.</div>
+    <div v-if="selectedTab === 'React'">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel cum saepe voluptas ipsum quisquam obcaecati?</div>
+  </Tabs>
+</template>
